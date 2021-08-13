1805f9e09dc9580eede5aa35e869048577289c20
David Lee <leedavid@microsoft.com>
2021-08-13 09:27:45 -0500

    resolve ssh

--- Deployment/Custom.ps1
+++ Deployment/Custom.ps1
@@ -24,25 +24,15 @@ if ($state -ne "Installed") {
     New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
     Stop-Service sshd
 
-    #New-Item -Path "C:\Users\$Username\.ssh" -ItemType Directory -Force    
-    #Copy-Item -Path $PubKeyFile -Destination "C:\Users\$Username\.ssh\authorized_keys" -Force
     Copy-Item -Path $PubKeyFile -Destination "C:\ProgramData\ssh\administrators_authorized_keys" -Force
 
     $content = Get-Content "C:\ProgramData\ssh\sshd_config"
     $content = $content.Replace("#PubkeyAuthentication yes", "PubkeyAuthentication yes")
     $content = $content.Replace("#PasswordAuthentication yes", "PasswordAuthentication no")
-    #$content = $content.Replace("Match Group administrators", "#Match Group administrators")
-    #$content = $content.Replace("       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys", "#       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys")
+    $content = $content.Replace("#StrictModes yes", "StrictModes no")
 
     Set-Content -Path "C:\ProgramData\ssh\sshd_config" -Value $content
 
-    # $acl = Get-Acl "C:\Users\$Username\.ssh\authorized_keys"
-    # $acl.SetAccessRuleProtection($true, $false)
-    # $administratorsRule = New-Object system.security.accesscontrol.filesystemaccessrule("Administrators", "FullControl", "Allow")
-    # $systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYSTEM", "FullControl", "Allow")
-    # $acl.SetAccessRule($administratorsRule)
-    # $acl.SetAccessRule($systemRule)
-    # $acl | Set-Acl
     Start-Service sshd
 }
 else {
